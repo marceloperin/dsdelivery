ec193157f358276d8438f0eda3039501632450bf
Jaime Eduardo <marcelo.pborba@gmail.com>
2021-01-09 15:56:47 -0300

    Fetch products

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,10 +1,28 @@
+
+import { useEffect, useState } from 'react';
+import { createNoSubstitutionTemplateLiteral } from 'typescript';
+import { fetchProducts } from '../api';
+import ProductsList from './ProductList';
 import StepsHeader from './StepsHeader';
 import './styles.css';
+import { Product } from './types';
 
 function Orders() {
+
+    const [products, setProducts] = useState<Product[]>([]);
+
+    console.table(products);
+
+    useEffect(() => {
+        fetchProducts()
+            .then(response => setProducts(response.data))
+            .catch(error => console.error(error))
+    }, []);
+
     return (
         <div className="orders-container">
             <StepsHeader />
+            <ProductsList products={products} />
         </div>
     )
 }
