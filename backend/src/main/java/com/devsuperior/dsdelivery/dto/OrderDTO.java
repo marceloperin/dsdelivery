fe3a27124757c03806019abe226a768fac115093
Jaime Eduardo <marcelo.pborba@gmail.com>
2021-01-12 13:18:08 -0300

    Include total value on orders

--- backend/src/main/java/com/devsuperior/dsdelivery/dto/OrderDTO.java
+++ backend/src/main/java/com/devsuperior/dsdelivery/dto/OrderDTO.java
@@ -19,19 +19,21 @@ public class OrderDTO implements Serializable {
 	private Double longitude;
 	private Instant moment;
 	private OrderStatus status;
+	private Double total;
 	
 	private List<ProductDTO> products = new ArrayList<>();
 	
 	public OrderDTO() {	
 	}
 
-	public OrderDTO(Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status) {
+	public OrderDTO(Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status, Double total) {
 		this.id = id;
 		this.address = address;
 		this.latitude = latitude;
 		this.longitude = longitude;
 		this.moment = moment;
 		this.status = status;
+		this.total = total;
 	}
 	
 	
@@ -42,6 +44,7 @@ public class OrderDTO implements Serializable {
 		this.longitude = entity.getLongitude();
 		this.moment = entity.getMoment();
 		this.status = entity.getStatus();
+		this.total = entity.getTotal();
 		
 		products = entity.getProducts().stream().map(p -> new ProductDTO(p)).collect(Collectors.toList());
 	}
@@ -96,6 +99,14 @@ public class OrderDTO implements Serializable {
 
 	public List<ProductDTO> getProducts() {
 		return products;
+	}
+
+	public Double getTotal() {
+		return total;
+	}
+
+	public void setTotal(Double total) {
+		this.total = total;
 	}
 
 }
